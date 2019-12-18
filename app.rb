997a3cdf0632f2ae72500f1612d53fccc2cb71d1
Alina Kornilova <alinakornilova78@gmail.com>
2019-12-18 15:13:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/' do
+    erb :user_input.erb
+  end
+  
+  post '/' do
+  @analyzed_text = TextAnalyzer.new(params[:user_input])
+ 
+  erb :results
+end
 end
